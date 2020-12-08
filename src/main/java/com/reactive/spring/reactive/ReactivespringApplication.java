580c6f5355de5b47f836e8d3b0763425978fbd74
adarshp14 <adarshpandey14@yahoo.com>
2020-12-08 20:49:52 +0530

    first commit

---
+++ src/main/java/com/reactive/spring/reactive/ReactivespringApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivespringApplication.class, args);
+	}
+
+}
